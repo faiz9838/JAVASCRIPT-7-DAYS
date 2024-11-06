6eb0e812e645a0c7f6fb8b6a03d40b298f654bf8
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-11-06 14:32:51 +0530

    first commit

---
+++ lesson-1.js
@@ -0,0 +1,58 @@
+// Objects Example
+
+// An empty object(“empty cabinet”) can be created using one of two syntaxes:
+
+// let user = new Object(); // "object constructor" syntax
+// let user = {}; //Using template Literal
+// console.log(user)
+
+// Example - 1
+// let user = new Object();
+// user = {
+//     name: "Faiz Ahmad",
+//     Age: 22
+// }
+// console.log(user)
+
+
+// Example - 2
+// let user = {
+//     name: "Faiz Ahmad",
+//     Age: 22
+// }
+// console.log(user.name)
+
+//Using square bracket
+
+// let user = {
+//     name: "Faiz Ahmad",
+//     ["class"]: 5
+// }
+// console.log(user.class)
+
+//Declaration of methods
+
+// let user = {
+//     name: 'Faiz Ahmad',
+//     age: 22,
+//     Goals: function () {
+//         console.log("Became a developer")
+//     }
+// }
+// user.Goals()
+
+// // TASKS
+
+// let user = {}
+// user = {
+//     name: "John",
+//     surname: "Smith",
+// }
+
+// user.name = "Pete"
+// delete user.name
+// console.log(user);
+
+
+
+
