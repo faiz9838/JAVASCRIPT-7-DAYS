50786ca26be5d5903cc316b05eb247662d7f034e
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-11-06 14:51:46 +0530

    updated

--- lesson-2.js
+++ lesson-2.js
@@ -1,1 +1,20 @@
 // Object Refrence
+
+// let a = {};
+// let b = a; // copy the reference
+
+// console.log(a == b);
+// console.log(a === b);
+
+//Assigning
+// let user = {}
+// Object.assign(user, { name: "Faiz Ahmad" })
+// // console.log(user);
+
+// //Cloning
+
+// let anotherUser = Object.assign({}, user)
+// console.log(anotherUser);
+
+
+
